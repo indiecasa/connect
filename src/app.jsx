bf04681187a74e2d8745dcfab765234d43b6e6aa
Buck Tower <buck@bucktower.net>
2018-04-06 12:22:37 -0500

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <ListFilterableContacts />
+      </div>
+    );
+  }
+}
+
+class ListFilterableContacts extends React.Component {
+  render() {
+    return (
+      <div>
+        <BarTitle />
+        <ViewSplit />
+      </div>
+    );
+  }
+}
+
+class BarTitle extends React.Component {
+  render() {
+    return (
+      <div className="title-bar">
+        <h1 class="Logo">Connect</h1>
+        {/* <Search />
+        <BtnNew /> */}
+      </div>
+    );
+  }
+}
+
+class ViewSplit extends React.Component {
+  render() {
+    return (
+      <div>
+        <ListConnect />
+        <ViewDetail />
+      </div>
+    );
+  }
+}
+
+class ListConnect extends React.Component {
+  render() {
+    return (
+      <ul>
+        <li>James Dean</li>
+        <li>John Lennon</li>
+        <li>Paul McCartney</li>
+      </ul>
+    );
+  }
+}
+
+class ViewDetail extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Name</h3>
+        <p>James Dean</p>
+        <h3>Phone</h3>
+        <p>832-394-9203</p>
+      </div>
+    )
+  }
+}
