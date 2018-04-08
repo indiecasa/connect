5eb4d4e0ee702c63149f42f0a5386a5ae2c8e8db
Buck Tower <buck@bucktower.net>
2018-04-08 07:41:31 -0500

    Begin work on detail view

--- src/app.jsx
+++ src/app.jsx
@@ -95,7 +95,7 @@ function ListItemConnect(props) {
   )
 }
 
-const listItems = contacts.map((c) =>
+const listConnectItems = contacts.map((c) =>
   <ListItemConnect firstName={c.firstName} lastName={c.lastName}
     loc={c.loc} org={c.org} key={c.uid} imgPath={c.imgPath}/>
 );
@@ -104,9 +104,34 @@ class ListConnect extends React.Component {
   render() {
     return (
       <ul className="connect-list">
-        {listItems}
+        {listConnectItems}
       </ul>
     );
+  }
+}
+
+function aggContactDetails(c) {
+  var ret = []
+  var ignore = ["uid", "firstName", "lastName", "org", "loc", "imgPath"]
+  for (var x in c) {
+    if (ignore.indexOf(x) == -1) {
+      ret.push(x);
+    }
+  }
+  return ret;
+}
+
+const listDetailItems = aggContactDetails(contacts[1]).map((d) =>
+  <p>{d}</p>
+);
+
+class ListDetail extends React.Component {
+  render() {
+    return (
+      <table>
+        {listDetailItems}
+      </table>
+    )
   }
 }
 
@@ -114,10 +139,20 @@ class ViewDetail extends React.Component {
   render() {
     return (
       <div className="detail-view">
+        <img src={contacts[0].imgPath} className="detail-pic" />
+        <h3>{contacts[1].firstName} {contacts[1].lastName}</h3>
+        <p className="list-org-active">{contacts[1].org}</p>
+        <br />
+        <svg width="8px" height="12px" viewBox="0 0 8 12" fill="#535353">
+          <path d="M4,12 L3.78159319,11.7280453 C3.62722351,11.5359608 0,6.98996122 0,4.2674441 C0,1.91457411 1.79427688,0 4,0 C6.20572312,0 8,1.91457411 8,4.2674441 C8,6.98996122 4.37277649,11.5359608 4.21840681,11.7280453 L4,12 L4,12 Z M4,0.613878269 C2.11136891,0.613878269 0.575406032,2.25286724 0.575406032,4.2674441 C0.575406032,6.40380649 3.2133024,10.0174372 4,11.0474986 C4.78638824,10.0174372 7.42459397,6.40248632 7.42459397,4.2674441 C7.42459397,2.25286724 5.88863109,0.613878269 4,0.613878269 L4,0.613878269 Z" id="Fill-55"></path>
+          <path d="M4,6 C2.8969574,6 2,5.10263692 2,4 C2,2.89736308 2.8969574,2 4,2 C5.1030426,2 6,2.89736308 6,4 C6,5.10263692 5.1030426,6 4,6 L4,6 Z M4,3 C3.44885993,3 3,3.4485342 3,4 C3,4.5514658 3.44885993,5 4,5 C4.55114007,5 5,4.5514658 5,4 C5,3.4485342 4.55114007,3 4,3 L4,3 Z" id="Fill-56"></path>
+        </svg>
+        <p className="list-loc">{contacts[1].loc}</p>
         <h3>Name</h3>
         <p>{contacts[0].firstName}</p>
         <h3>Phone</h3>
         <p>832-394-9203</p>
+        <ListDetail />
       </div>
     )
   }
